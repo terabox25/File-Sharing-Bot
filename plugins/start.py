97416d2e00d1819318ac77872da84c5872f3d1df
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-15 08:00:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,27 +20,9 @@ file_auto_delete = humanize.naturaldelta(jishudeveloper)
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    data = message.command[1]
-    if data.split("-", 1)[0] == "verify": # set if or elif it depend on your code
-        userid = data.split("-", 2)[1]
-        token = data.split("-", 3)[2]
-        if str(message.from_user.id) != str(userid):
-            return await message.reply_text(
-                text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
-            )
-        is_valid = await check_token(client, userid, token)
-        if is_valid == True:
-            await message.reply_text(
-                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all files till today midnight.</b>",
-                protect_content=True
-            )
-            await verify_user(client, userid, token)
-        else:
-            return await message.reply_text(
-                text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
-            )
+    
+ 
+            
     id = message.from_user.id
     if not await present_user(id):
         try:
